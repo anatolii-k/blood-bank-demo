b9976bb6ff58e63a60903f44a4074c82eeb361c7
anatolii-k <anatoly.zp@gmail.com>
2025-03-12 16:21:07 +0200

    Initial commit. 1. Main HTML page 2. Basic Angular project for Distribution project 3. Basic Spring Boot (from Initialzr)

---
+++ src/main/java/anatolii/k/bloodbank/BloodBankSystemApplication.java
@@ -0,0 +1,13 @@
+package anatolii.k.bloodbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankSystemApplication.class, args);
+	}
+
+}
